d9d09ed9810875becc04eeb9e3eb11c962c14490
A-userGit <asemchenok@gmail.com>
2023-08-13 14:59:40 +0300

    save1

--- src/main/java/com/quest/dao/repositries/MapDataRepository.java
+++ src/main/java/com/quest/dao/repositries/MapDataRepository.java
@@ -3,6 +3,9 @@ package com.quest.dao.repositries;
 import com.quest.dao.interfaces.MapDataDao;
 import com.quest.dao.models.MapNodeEntity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
@@ -12,17 +15,22 @@ public class MapDataRepository implements MapDataDao {
 
     MapDataRepository()
     {
-        propertiesAccessPoint = new Properties("")
+        propertiesAccessPoint = new Properties();
+        try {
+            propertiesAccessPoint.load(new FileInputStream(new File("datarepository.properties")));
+        } catch (IOException e) {
+
+        }
     }
 
     @Override
     public List<MapNodeEntity> getMap() {
+
         return null;
     }
 
     @Override
     public boolean saveNode(MapNodeEntity entity) {
 
-        return true;
     }
 }
